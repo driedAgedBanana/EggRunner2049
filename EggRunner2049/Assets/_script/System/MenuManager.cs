55be7bd370e112422596de928f193940475e96d1
dryAgedBanana <127958189+driedAgedBanana@users.noreply.github.com>
2025-04-14 23:55:54 +0200

    Adding simple tutorial

--- EggRunner2049/Assets/_script/System/MenuManager.cs
+++ EggRunner2049/Assets/_script/System/MenuManager.cs
@@ -6,22 +6,53 @@ public class MenuManager : MonoBehaviour
 {
     public GameObject WelcomeScene;
     public GameObject MainScene;
+    public GameObject TutorialAsk;
+    public GameObject TutorialPanel;
+
+    private bool _hasStarted = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         WelcomeScene.SetActive(true);
         MainScene.SetActive(false);
+        TutorialAsk.SetActive(false);
+        TutorialPanel.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.anyKey)
+        if(Input.anyKey && !_hasStarted)
         {
             WelcomeScene.SetActive(false);
             MainScene.SetActive(true);
+            _hasStarted = true;
         }
+    }
+
+    public void ToTutorialAsk()
+    {
+        WelcomeScene.SetActive(false);
+        MainScene.SetActive(false);
+        TutorialPanel.SetActive(false);
+        TutorialAsk.SetActive(true);
+    }
+
+    public void ToTutorialPanel()
+    {
+        WelcomeScene.SetActive(false);
+        MainScene.SetActive(false);
+        TutorialAsk.SetActive(false);
+        TutorialPanel.SetActive(true);
+    }
+
+    public void BackToMenu()
+    {
+        WelcomeScene.SetActive(false);
+        MainScene.SetActive(true);
+        TutorialAsk.SetActive(false);
+        TutorialPanel.SetActive(false);
     }
 
     public void ToArena()
